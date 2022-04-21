8327566297aee14d031f38db53d4bac8e97fa7b2
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-04-21 00:05:18 -0300

    Alteração de planos

--- config/products.js
+++ config/products.js
@@ -1,48 +1,50 @@
 module.exports = {
-    USD:[
-        {
-            id: 1,
-            titulo: "1 Month",
-            descricao: "1 Month of Mikrotik OpenVPN Config Generator premium access",
-            "preco": process.env.PAYPAL_USD_ONE_MONTH,
-            "dias": 30
-        },
-        {
-            id: 2,
-            titulo: "3 Months",
-            descricao: "3 Months of Mikrotik OpenVPN Config Generator premium access",
-            preco: process.env.PAYPAL_USD_THREE_MONTHS,
-            dias: 90
-        },
-        {
-            id: 3,
-            titulo: "6 Months",
-            descricao: "6 Months of Mikrotik OpenVPN Config Generator premium access",
-            preco: process.env.PAYPAL_USD_SIX_MONTHS,
-            dias: 180
-        }
-    ],
-    BRL:[
-        {
-            id: 1,
-            titulo: "1 Mes",
-            descricao: "1 Mes de acesso premium no Mikrotik OpenVPN Config Generator",
-            preco: process.env.PAYPAL_BRL_ONE_MONTH,
-            dias: 30
-        },
-        {
-            id: 2,
-            titulo: "3 Meses",
-            descricao: "3 Meses de acesso premium no Mikrotik OpenVPN Config Generator",
-            preco: process.env.PAYPAL_BRL_THREE_MONTHS,
-            dias: 90
-        },
-        {
-            id: 3,
-            titulo: "6 Meses",
-            descricao: "6 Meses de acesso premium no Mikrotik OpenVPN Config Generator",
-            preco: process.env.PAYPAL_BRL_SIX_MONTHS,
-            dias: 180
-        }
-    ]
-}
+  USD: [
+    {
+      id: 2,
+      titulo: "3 Months",
+      descricao: "3 Months of Mikrotik OpenVPN Config Generator premium access",
+      preco: process.env.PAYPAL_USD_THREE_MONTHS,
+      dias: 90,
+    },
+    {
+      id: 3,
+      titulo: "6 Months",
+      descricao: "6 Months of Mikrotik OpenVPN Config Generator premium access",
+      preco: process.env.PAYPAL_USD_SIX_MONTHS,
+      dias: 180,
+    },
+    {
+      id: 4,
+      titulo: "1 Year",
+      descricao: "1 Year of Mikrotik OpenVPN Config Generator premium access",
+      preco: process.env.PAYPAL_USD_ONE_YEAR,
+      dias: 365,
+    },
+  ],
+  BRL: [
+    {
+      id: 2,
+      titulo: "3 Meses",
+      descricao:
+        "3 Meses de acesso premium no Mikrotik OpenVPN Config Generator",
+      preco: process.env.PAYPAL_BRL_THREE_MONTHS,
+      dias: 90,
+    },
+    {
+      id: 3,
+      titulo: "6 Meses",
+      descricao:
+        "6 Meses de acesso premium no Mikrotik OpenVPN Config Generator",
+      preco: process.env.PAYPAL_BRL_SIX_MONTHS,
+      dias: 180,
+    },
+    {
+      id: 4,
+      titulo: "1 Ano",
+      descricao: "1 ano de acesso premium no Mikrotik OpenVPN Config Generator",
+      preco: process.env.PAYPAL_USD_ONE_YEAR,
+      dias: 365,
+    },
+  ],
+};
